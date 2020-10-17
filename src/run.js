5744a6efacc1aac0aa8209cf037bcd7c029d48fa
Ian Dela Cruz <iandc76@gmail.com>
2020-10-17 10:13:13 +0800

    Add secret install

--- src/run.js
+++ src/run.js
@@ -1,8 +1,10 @@
 import gpg from './gpg';
+import secret from './secret';
 import secrets from './secrets';
 
 const run = async () => {
   await gpg.install();
+  await secret.install();
   await secrets.install();
 };
 
