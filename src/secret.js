ad9c7a9b4b8197fb4d9c94e04ad6a4f898295b1f
Ian Dela Cruz <iandc76@gmail.com>
2020-10-17 10:12:20 +0800

    Add git-secret

---
+++ src/secret.js
@@ -0,0 +1,25 @@
+// Encrypt files that containt passwords and other sensitive information
+import cmd from './cmd';
+
+/**
+ * brew install git-secret
+ */
+const install = async () => {
+  try {
+    await cmd('brew', ['list', 'git-secret'], false);
+    console.log('git-secret already installed');
+  } catch (error) {
+    return cmd('brew', ['install', 'git-secret']);
+  }
+};
+
+/**
+ * git secret init
+ * See README.md on how to use
+ */
+const setup = async () => {
+  await cmd('git', ['secret', 'init']);
+  console.log('Visit https://git-secret.io/ for more info.');
+};
+
+export default { install, setup };
